2d7cd733aae3b23eaa78d48aa11bb511f47fb941
manav0311 <manav.shah81@gmail.com>
2019-09-30 18:00:06 +0530

    Recycler View

--- app/src/main/java/com/example/myapp/uploadPdf.java
+++ app/src/main/java/com/example/myapp/uploadPdf.java
@@ -1,24 +1,45 @@
 package com.example.myapp;
 
+import android.widget.RadioButton;
+
 public class uploadPdf {
     public String name;
     public String url;
+    public String created;
+    public String uploaded;
+    public String tag;
+    public RadioButton bookmark;
 
-    public uploadPdf() {
-    }
-
-    public uploadPdf(String name, String url) {
+    public uploadPdf(String name, String url, String created, String uploaded, String tag, RadioButton bookmark) {
         this.name = name;
         this.url = url;
+        this.created = created;
+        this.uploaded = uploaded;
+        this.tag = tag;
+        this.bookmark = bookmark;
     }
 
     public String getName() {
         return name;
     }
 
-
     public String getUrl() {
         return url;
     }
 
+    public String getCreated() {
+        return created;
+    }
+
+    public String getUploaded() {
+        return uploaded;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public RadioButton getBookmark() {
+        return bookmark;
+    }
 }
